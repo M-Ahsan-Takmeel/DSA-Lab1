f15c3d9554b581c020cd0d169aff4b717a549a6b
M-Ahsan-Takmeel <143287157+M-Ahsan-Takmeel@users.noreply.github.com>
2026-09-09 18:16:26 +0500

    Add memory management for Pascal's Triangle
    Added a function to free the allocated memory for Pascal's Triangle.

--- task_5_function.h
+++ task_5_function.h
@@ -1,18 +1,30 @@
 #ifndef PASCALS_TRIANGLE_H
 #define PASCALS_TRIANGLE_H
 using namespace std;
-// Generates Pascal's Triangle using a 2D dynamic array
-inline int** generatePascalsTriangle(int n) {
+inline int** generatePascalsTriangle(int n) 
+{
     if (n <= 0) return nullptr;
     int** triangle = new int*[n];
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i) 
+    {
         triangle[i] = new int[i + 1];
         triangle[i][0] = 1;
         triangle[i][i] = 1;
-        for (int j = 1; j < i; ++j) {
+        for (int j = 1; j < i; ++j) 
+        {
             triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
         }
     }
     return triangle;
 }
+inline void freeTriangle(int** triangle, int n) 
+{
+    if (!triangle) return;
+    for (int i = 0; i < n; ++i) 
+    {
+        delete[] triangle[i];
+    }
+    delete[] triangle;
+}
+
 #endif
